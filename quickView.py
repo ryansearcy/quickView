90f69bb67abc23e6398f86b0e61609b1b52f4fb6
Ryan Searcy <rvsearcy1@gmail.com>
2026-04-07 14:45:37 -0400

    PATHv1.0 - changed logic to pull PATH data

--- quickView.py
+++ quickView.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, request, redirect, render_template, url_for
-import string, random, base64, requests, json, time, operator
+import string, random, base64, requests, json, time, operator, math, re
 from json import JSONDecodeError
 from datetime import timedelta, datetime
 from google.transit import gtfs_realtime_pb2
@@ -8,6 +8,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 quickView = Flask(__name__)
 
@@ -40,9 +44,123 @@ lineData: list[dict[str, str]] = emptyTypes[list]
 lineMapping: dict[tuple[str, str], str] = emptyTypes[dict]
 lineList: list[str] = emptyTypes[list]
 mtaAPIURIs: list[str] = emptyTypes[list]
+schedule: dict = {}
 
 apiVariables: list[str] = emptyTypes[list]
-appVariables: list[str] = spotifyAPIVariables + spotifyTokenVariables + transitVariables + ['apiVariables'] + ['lineMapping', 'lineList']
+appVariables: list[str] = spotifyAPIVariables + spotifyTokenVariables + transitVariables + ['apiVariables'] + ['lineMapping', 'lineList'] + ['schedule']
+
+class HtmlTable(object):
+    '''
+    HtmlTable
+    '''
+
+    def __init__(self, url):
+        '''
+        Constructor
+        '''
+        firefoxOptions = Options()
+        firefoxOptions.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
+        firefoxOptions.add_argument('--headless')
+        firefoxOptions.add_argument('--disable-gpu')
+        firefoxDriver = webdriver.Firefox(options=firefoxOptions)
+        firefoxDriver.get(url)
+        time.sleep(1)
+        page = firefoxDriver.page_source
+        firefoxDriver.quit()
+        #self.html_page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        #print(requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text)
+
+        self.soup = BeautifulSoup(page, 'html.parser')
+        #self.body = self.soup.find("table")
+        #print(self.body)
+
+    def get_weekday_tables(self,header_tag:str=None)->dict:
+        
+        tables = {'Weekday':{}}
+        for i,table in enumerate(self.soup.find_all("table")):
+            fields = []
+            record= {}
+            for td in table.find_all('tr', recursive=True)[0].find_all('td', recursive=True):
+                    text = re.sub("\n", "", td.text)
+                    label = re.search(".+?(?=Arrival)|.+?(?=Departure)|(?<=Arrival at).*", text)[0].strip()
+                    label = re.sub("\\xa0", "", label)
+                    label = re.sub("Exchange", "Exchange Place", label)
+                    label = re.sub("33 St", "33S", label)
+                    label = re.sub("\\.", "", label)
+                    fields.append(label)
+                    record[label] = []
+            #print(fields)
+            for i, tr in enumerate(table.find_all('tr', recursive=True)):
+                if i == 0:
+                        continue
+                for j, td in enumerate(tr.find_all('td', recursive=True)):
+                    record[fields[j]].append(td.text)
+            #print(record)
+                if fields[len(fields)-1] not in tables:
+                    tables['Weekday'][fields[len(fields)-1]] = {}
+                    tables['Weekday'][fields[len(fields)-1]][fields[0]] = record
+                else:
+                    if fields[0] in tables['Weekday'][fields[len(fields)-1]]:
+                        continue
+                    else:
+                        tables['Weekday'][fields[len(fields)-1]][fields[0]] = record    
+            
+        return tables
+
+    def get_weekend_tables(self,header_tag:str=None)->dict:
+
+        tables = {'Saturday':{}, 'Sunday':{}}
+        k = 0
+        c1 = 0
+        c2 = 0
+        for i,table in enumerate(self.soup.find_all("table")):
+            fields = []
+            record= {}
+            for td in table.find_all('tr', recursive=True)[0].find_all('td', recursive=True):
+                    text = re.sub("\n", "", td.text)
+                    label = re.search(".+?(?=Arrival)|.+?(?=Departure)|(?<=Arrival at).*", text)[0].strip()
+                    label = re.sub("\\xa0", "", label)
+                    label = re.sub("Exchange", "Exchange Place", label)
+                    label = re.sub("33 St", "33S", label)
+                    label = re.sub("\\.", "", label)
+                    fields.append(label)
+                    record[label] = []
+            #print(fields)
+            for i, tr in enumerate(table.find_all('tr', recursive=True)):
+                if i == 0:
+                        continue
+                for j, td in enumerate(tr.find_all('td', recursive=True)):
+                    record[fields[j]].append(td.text)
+            #print(record)
+            if k == 0:
+                if fields[len(fields)-1] not in tables['Saturday']:
+                    tables['Saturday'][fields[len(fields)-1]] = {}
+                    tables['Saturday'][fields[len(fields)-1]][fields[0]] = record
+                    c1+=1
+                else:
+                    if fields[0] in tables['Saturday'][fields[len(fields)-1]]:
+                        tables['Sunday'][fields[len(fields)-1]] = {}
+                        tables['Sunday'][fields[len(fields)-1]][fields[0]] = record
+                        k+=1
+                        c2+=1
+                        continue
+                    else:
+                        tables['Saturday'][fields[len(fields)-1]][fields[0]] = record
+                        c1+=1
+            if k == 1 and c2 < c1:
+                if fields[len(fields)-1] not in tables['Sunday']:
+                    tables['Sunday'][fields[len(fields)-1]] = {}
+                    tables['Sunday'][fields[len(fields)-1]][fields[0]] = record
+                    c2+=1
+                else:
+                    if fields[0] in tables['Sunday'][fields[len(fields)-1]]:
+                        print(fields[0] + ' - ' + fields[len(fields)-1])
+                        break
+                    else:
+                        tables['Sunday'][fields[len(fields)-1]][fields[0]] = record
+                        c2+=1
+            
+        return tables
 
 def checkForEmptyGlobalVariables(variableNames: list[str] | str) -> bool:
     if type(variableNames) == str:
@@ -101,6 +219,16 @@ def writeJSONVariables(variables: dict[str, Any]) -> None:
     jsonFile.close()
     return
 
+def fetchSchedules() -> None:
+    weekday_url="https://www.panynj.gov/path/en/schedules-maps/weekday-schedules.html"
+    weekday_schedule=HtmlTable(weekday_url)
+    weekday_schedule=weekday_schedule.get_weekday_tables()
+    weekend_url="https://www.panynj.gov/path/en/schedules-maps/weekend-schedules.html"
+    weekend_schedule=HtmlTable(weekend_url)
+    weekend_schedule=weekend_schedule.get_weekend_tables()
+    setGlobalVariable('schedule', weekday_schedule | weekend_schedule)
+    return
+
 def genRandomString(length: int) -> str:
     letters = string.ascii_letters + string.digits
     return emptyString.join(random.choice(letters) for i in range(length))
@@ -122,11 +250,11 @@ def refreshAuthToken() -> int:
     authResponse = requests.post('https://accounts.spotify.com/api/token', data=formData, headers=headers)
     if authResponse.status_code == 200:
         authJSON = authResponse.json()
-        #quickView.logger.debug('Refresh data: ' + authJSON.__str__())
         setGlobalVariable('accessToken', authJSON['access_token'])
         if 'refresh_token' in authJSON:
             setGlobalVariable('refresh_token', authJSON['refresh_token'])
         setGlobalVariable('expiresAt', datetime.today() + timedelta(seconds=(round(authJSON['expires_in']*0.75))))
+    fetchSchedules()
     return authResponse.status_code
 
 @quickView.route("/")
@@ -137,12 +265,13 @@ def genAuthCode() -> Response:
     lineData.sort(key=operator.itemgetter('priority'))
     setGlobalVariable('lineMapping', {(l['line'], l['stop']): l['priority'] for l in lineData})
     setGlobalVariable('lineList', list(set([l[0] for l in lineMapping])))
+   # fetchSchedules()
     state: str = genRandomString(16)
+    quickView.logger.debug('Starting firefox headless')
     firefoxOptions = Options()
     firefoxOptions.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
     firefoxOptions.add_argument('--headless')
     firefoxDriver = webdriver.Firefox(options=firefoxOptions)
-    quickView.logger.debug('Routing to Spotify login page')
     firefoxDriver.get('https://accounts.spotify.com/en/login?allow_password=1')
     spotifyUsernameInput = firefoxDriver.find_element(value='username')
     spotifyUsernameInput.send_keys(spotifyUsername)
@@ -176,10 +305,8 @@ def getSpotifyPlaybackData() -> dict:
         return {'status_code': 401, 'is_playing': False}
     if expiresAt <= datetime.today():
         refreshAuthToken()
-    #quickView.logger.debug('accessToken Value: ' + accessToken)
     playbackHeaders: dict = {'Authorization':'Bearer ' + accessToken}
     playbackInfo: Response = requests.get('https://api.spotify.com/v1/me/player', headers=playbackHeaders)
-    #quickView.logger.debug('Playback State Response Code: ' + playbackInfo.status_code.__str__())
     if playbackInfo.status_code == 200:
         playbackData = playbackInfo.json()
         quickView.logger.debug('Device ID: ' + playbackData['device']['name'])
@@ -239,50 +366,38 @@ def nextSong():
         quickView.logger.debug('Error when previous: ' + nextSuccess.status_code.__str__())
     return {'status': nextSuccess.status_code}
 
-def fetchTransitTimes(apiURIs: list, lineData: list) -> dict:
-    transitFeed = gtfs_realtime_pb2.FeedMessage()
-    arrivalTimes = {line['priority']: {'type': line['type'], 'line': line['line'], 'destination': line['destination'], 'times': []} for line in lineData}
-    lineKeys = lineMapping.keys()
-    for uri in apiURIs:
-        transitTimes = requests.get(uri)
-        transitFeed.ParseFromString(transitTimes.content)
-        for entity in transitFeed.entity:
-            if entity.HasField('trip_update') != True:
-                continue
-            routeID = entity.trip_update.trip.route_id
-            if routeID not in lineList:
-                continue
-            for stopTimeUpdate in entity.trip_update.stop_time_update:
-                stopID = stopTimeUpdate.stop_id
-                if (routeID, stopID) not in lineKeys:
-                    continue
-                minutesAway = round((datetime.fromtimestamp(stopTimeUpdate.arrival.time) - datetime.now()).total_seconds() / 60)
-                minTime = lineData[lineMapping[(routeID, stopID)]]['min']
-                maxTime = lineData[lineMapping[(routeID, stopID)]]['max']
-                if minutesAway > maxTime or minutesAway < minTime:
-                    continue
-                arrivalTimes[lineMapping[(routeID, stopID)]]['times'].append(minutesAway)
-    for line in arrivalTimes:
-        timesLength: int = len(arrivalTimes[line]['times'])
-        if  timesLength == 0:
-            arrivalTimes[line]['times'] = 'No Departures'
-        else:
-            if timesLength > 4:
-                arrivalTimes[line]['times'] = arrivalTimes[line]['times'][:4]
-            arrivalTimes[line]['times'].sort()
-            arrivalTimes[line]['times'] = ', '.join([str(times) for times in arrivalTimes[line]['times']])
-    return arrivalTimes
+# def addScheduledTimes(realTimeTransit: dict):
+#     for i in realTimeTransit:
+#         for j in realTimeTransit[i]:
+#             if len(j) < 1 or len([t for time in realTimeTransit[i][j] if time >= 10]) == 0:
+#                realTimeTransit[i][j] = realTimeTransit[i][j] + [t for  ]
+                
+def fetchTransitTimes() -> dict:
+    pathJSON = requests.get("https://www.panynj.gov/bin/portauthority/ridepath.json", headers={'User-Agent':'Firefox'}).json()
+    groveSt = [destinationData['messages'] for destinationData in [stationData['destinations'] for stationData in pathJSON['results'] if stationData['consideredStation'] == 'GRV'][0] if destinationData['label'] == 'ToNY'][0]
+    exchangePlace = [destinationData['messages'] for destinationData in [stationData['destinations'] for stationData in pathJSON['results'] if stationData['consideredStation'] == 'EXP'][0] if destinationData['label'] == 'ToNY'][0]
+    pathTimes = {'Grove St':{'WTC':{}, '33S':{}}, 'Exchange Place':{'WTC':{}}}
+    mappings = {'Grove St':groveSt, 'Exchange Place':exchangePlace}
+    for i in mappings:
+        for j in mappings[i]:
+            if pathTimes[i][j['target']] != {}:
+                pathTimes[i][j['target']]['tTA'] = pathTimes[i][j['target']]['tTA'] + ', ' + str(math.floor(int(j['secondsToArrival'])/60)) + ' mins'
+                if pathTimes[i][j['target']]['sTA'] > int(j['secondsToArrival']):
+                    pathTimes[i][j['target']]['hS'] = j['headSign']
+            else:
+                pathTimes[i][j['target']]['sTA'] = int(j['secondsToArrival'])
+                pathTimes[i][j['target']]['tTA'] = str(math.floor(int(j['secondsToArrival'])/60)) + ' mins'
+                pathTimes[i][j['target']]['hS'] = j['headSign']
+    return pathTimes
 
 @quickView.route("/realtime-transit")
 def realtimeTransit() -> str:
     if checkForEmptyGlobalVariables(spotifyTokenVariables):
         return redirect(url_for('genAuthCode'))
-    transitTimes = fetchTransitTimes(mtaAPIURIs, lineData)
-    #quickView.logger.debug('Successfully fetched transit times')
+    transitTimes = fetchTransitTimes()
     return render_template('transit.html', lines=transitTimes, keep_trailing_newline=True)
 
 @quickView.get("/transit-times")
 def fetchTimes() -> str:
-    transitTimes = fetchTransitTimes(mtaAPIURIs, lineData)
-    #quickView.logger.debug('Transit Times: ' + json.dumps(transitTimes))
+    transitTimes = fetchTransitTimes()
     return json.dumps(transitTimes)
